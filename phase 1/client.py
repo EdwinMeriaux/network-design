b1c367b98c6e1b94a6c8250c11ba1a3ddf645e34
EdwinMeriaux <Edwin_Meriaux@student.uml.edu>
2022-01-22 12:15:30 -0500

    phase 1 no echo

--- phase 1/client.py
+++ phase 1/client.py
@@ -3,15 +3,26 @@ import time
  
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+UDP_PORT2 = 5006
 MESSAGE = b"Hello, World!"
+
+sock2 = socket.socket(socket.AF_INET, # Internet
+                 socket.SOCK_DGRAM) # UDP
+sock2.bind((UDP_IP, UDP_PORT2))
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE)
 i = 0
 while i < 2:
+    print("start")
     sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    time.sleep(1)
+    print("sent")
     i = i + 1
+    data, addr = sock2.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
+    print("recieved")
+    time.sleep(1)
+print("done")
--- phase 1/server.py
+++ phase 1/server.py
@@ -3,14 +3,20 @@ import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 UDP_PORT2 = 5006
-
+MESSAGE = b"Hello, World!"
 sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 i = 0
 while i < 2:
+     print("start")
      data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
      print("received message: %s" % data)
+     print("recieved")
      i = i + 1
-     
+     sock2 = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+     sock2.sendto(MESSAGE, (UDP_IP, UDP_PORT2))
+     print("sent")
+print("done")
